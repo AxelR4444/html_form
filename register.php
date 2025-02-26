37de38f230b3fca097192d17c6adc90c9af70eca
COMLAB <ardungca23-0301@cca.edu.ph>
2025-02-26 19:37:46 +0800

    Update register.php

--- register.php
+++ register.php
@@ -3,8 +3,11 @@
         <title>Form</title>
     </head>
     <body>
+        <h1>ADD STUDENT</h1>
         <form action="action_page.php" method="POST">
-            <table border="1" cellspacing= "5">
+            <table border="1" cellpadding="20" cellspacing="5" width="60%">
+                <th>Question</th>
+                <th>Answer</th>
                 <tr>
                     <td> Name: </td>
                     <td> <input type="text" name="name"> </td>
@@ -12,7 +15,7 @@
                 <tr>
                     <td> Gender: </td>
                     <td>
-                        <input type="radio" name="gender" value="male"> Male
+                        <input type="radio" name="gender" value="male" checked> Male
                         <input type="radio" name="gender" value="female"> Female
                     </td>
                 </tr>
@@ -20,15 +23,15 @@
                     <td> Country: </td>
                     <td>
                         <select name="country">
-                            <option value="PH">Philippines</option>
+                            <option value="PH" select>Philippines</option>
                             <option value="AUS">Australia</option>
                             <option value="US">United States</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="2">
-                        <input type="submit" value="Save">
+                    <td colspan="2" align="right">
+                        <input type="submit" name="save" value="Save">
                     </td>
                 </tr>
             </table>
